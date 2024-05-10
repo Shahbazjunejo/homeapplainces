8f0c58b80fe923ac945e476cb81c27318f3f2387
Shahbaz Sharif <shahbaz.sharif@leopardscourier.com>
2024-05-10 19:56:14 +0500

    validation

--- lib/LoginScreen.dart
+++ lib/LoginScreen.dart
@@ -7,6 +7,11 @@ class LoginScreen extends StatelessWidget {
 
   final TextEditingController email = TextEditingController();
   final TextEditingController password= TextEditingController();
+   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
+   bool isEmailValid(String email) {
+     return RegExp(r'^[\w-\.]+@[a-zA-Z]+\.[a-zA-Z]{2,}$').hasMatch(email);
+   }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -16,108 +21,135 @@ class LoginScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(40.0),
-        child: Column(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const Text(
-              'Hello again!',
-              style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 8),
-            Text(
-              'Please login to your account',
-              style: TextStyle(color: Colors.grey[700]),
-              textAlign: TextAlign.center,
-            ),
-            const SizedBox(height: 32),
-            TextFormField(
-              controller: email,
-              decoration: InputDecoration(
-                labelText: 'Email',
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide.none,
-                ),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              const Text(
+                'Hello again!',
+                style: TextStyle(fontSize: 32, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
               ),
-            ),
-            const SizedBox(height: 16),
-            TextFormField(
-              controller: password,
-              obscureText: true,
-              decoration: InputDecoration(
-                labelText: 'Login',
-                filled: true,
-                fillColor: Colors.grey[200],
-                border: OutlineInputBorder(
-                  borderRadius: BorderRadius.circular(8),
-                  borderSide: BorderSide.none,
-                ),
+              const SizedBox(height: 8),
+              Text(
+                'Please login to your account',
+                style: TextStyle(color: Colors.grey[700]),
+                textAlign: TextAlign.center,
               ),
-            ),
-            const SizedBox(height: 16),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: [
-                TextButton(
-                  onPressed: () {
-                    // Handle 'Remember me'
-                  },
-                  child: const Text('Remember me'),
-                ),
-                TextButton(
-                  onPressed: () {
-                    // Handle 'Forgot password?'
-                  },
-                  child: const Text('Forgot password?'),
-                ),
-              ],
-            ),
-            const SizedBox(height: 16),
-            ElevatedButton(
-              onPressed: () {
-                if(email.text.isNotEmpty&&email.text=="admin"&&password.text.isNotEmpty&&password.text=="admin"){
-                  Navigator.of(context).pushNamed('/ServiceProvider');
-                }else{
-               loginUser(email.text, password.text, context);
-                }
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue,
-                padding: const EdgeInsets.symmetric(vertical: 12),
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(8),
-                ),
-              ),
-              child: const Text(
-                'Let\'s start!',
-                style: TextStyle(fontSize: 16,
-                    color: Colors.white),
-              ),
-            ),
-            const SizedBox(height: 30,),
-            const Center(
-              child: Text(
-                "Didn't Have an Account?",
-                style: TextStyle(fontSize: 16, color: Colors.black),
-              ),
-            ),
-            TextButton(
-                onPressed: () {
-                  Navigator.of(context).pushNamed('/SignupScreen');
-                },
-                child: const Text(
-                  "Signup",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.blue,
+              const SizedBox(height: 32),
+              TextFormField(
+                controller: email,
+                decoration: InputDecoration(
+                  labelText: 'Email',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide.none,
                   ),
-                )),
-          ],
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter  email address';
+                  }
+                  if (value.length < 4) {
+                    return 'please enter at least 4 characters';
+                  }
+                  return null;
+                },
+
+
+              ),
+              const SizedBox(height: 16),
+              TextFormField(
+                controller: password,
+                obscureText: true,
+                decoration: InputDecoration(
+                  labelText: 'Login',
+                  filled: true,
+                  fillColor: Colors.grey[200],
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                    borderSide: BorderSide.none,
+                  ),
+                ),
+                validator: (value) {
+                  if (value!.isEmpty) {
+                    return 'Please enter value';
+                  }
+                  if (value.length < 4) {
+                    return 'please enter at least 4 characters';
+                  }
+                  return null;
+                },
+
+              ),
+              const SizedBox(height: 16),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  TextButton(
+                    onPressed: () {
+                      // Handle 'Remember me'
+                    },
+                    child: const Text('Remember me'),
+                  ),
+                  TextButton(
+                    onPressed: () {
+                      // Handle 'Forgot password?'
+                    },
+                    child: const Text('Forgot password?'),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 16),
+              ElevatedButton(
+                onPressed: () {
+    if (_formKey.currentState!.validate()) {
+      if (email.text.isNotEmpty && email.text == "admin" &&
+          password.text.isNotEmpty && password.text == "admin") {
+        Navigator.of(context).pushNamed('/ServiceProvider');
+      } else {
+        loginUser(email.text, password.text, context);
+      }
+    }
+                },
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue,
+                  padding: const EdgeInsets.symmetric(vertical: 12),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                ),
+                child: const Text(
+                  'Let\'s start!',
+                  style: TextStyle(fontSize: 16,
+                      color: Colors.white),
+                ),
+              ),
+              const SizedBox(height: 30,),
+              const Center(
+                child: Text(
+                  "Didn't Have an Account?",
+                  style: TextStyle(fontSize: 16, color: Colors.black),
+                ),
+              ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/SignupScreen');
+                  },
+                  child: const Text(
+                    "Signup",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.blue,
+                    ),
+                  )),
+            ],
+          ),
         ),
       ),
     );
--- lib/SignupScreen.dart
+++ lib/SignupScreen.dart
@@ -11,7 +11,7 @@ class SignupScreen extends StatelessWidget {
    final TextEditingController fullname = TextEditingController();
    final TextEditingController emailaddress = TextEditingController();
    final TextEditingController phonenumber = TextEditingController();
-
+   final GlobalKey<FormState> _formKey = GlobalKey<FormState>();
 
    @override
   Widget build(BuildContext context) {
@@ -23,93 +23,96 @@ class SignupScreen extends StatelessWidget {
       ),
       body: Padding(
         padding: const EdgeInsets.all(40.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            const Text("Start Your Journey with Us!",
-              style: TextStyle(
-                fontSize: 28,
-                fontWeight: FontWeight.normal,
-              ),
-            ),
-            const SizedBox(height: 30,),
-            _buildTextField(hint: 'Username',controller: username),
-            const SizedBox(height: 20,),
-            _buildTextField(hint: 'Password', obscureText: true,controller: password),
-            const SizedBox(height: 20,),
-            _buildTextField(hint: 'Full name',controller: fullname),
-            const SizedBox(height: 20,),
-            _buildTextField(hint: 'Email address',controller: emailaddress),
-            const SizedBox(height: 20,),
-            _buildTextField(hint: 'Phone number',controller: phonenumber),
-            const SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () {
-                registerUser( username!.text,password!.text,fullname!.text,emailaddress!.text,phonenumber!.text);
-                username.clear();
-                password.clear();
-                fullname.clear();
-                emailaddress.clear();
-                phonenumber.clear();
-                showDialog(
-                  context: context,
-                  builder: (BuildContext context) {
-                    return AlertDialog(
-                      title: Text('Sign up '),
-                      content: Text('Sign up Successfully'),
-                      actions: <Widget>[
-                        TextButton(
-                          onPressed: () {
-                            Navigator.of(context).pop();
-                          },
-                          child: Text('OK'),
-                        ),
-                      ],
-                    );
-                  },
-                );
-              },
-              style: ElevatedButton.styleFrom(
-                backgroundColor: Colors.blue, // Background color
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(10.0),
+        child: Form(
+          key: _formKey,
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: <Widget>[
+              const Text("Start Your Journey with Us!",
+                style: TextStyle(
+                  fontSize: 28,
+                  fontWeight: FontWeight.normal,
                 ),
-                padding: const EdgeInsets.symmetric(vertical: 14.0),
               ),
-              child: const Text('Start', style: TextStyle(
-                fontSize: 22,
-                fontWeight: FontWeight.normal,
-                color: Colors.white,
-              ),),
-            ),
-            const SizedBox(
-              height: 30,
-            ),
-            const Center(
-              child: Text(
-                "Already Have an Account?",
-                style: TextStyle(fontSize: 16, color: Colors.black),
-              ),
-            ),
-            TextButton(
+              const SizedBox(height: 30,),
+              _buildTextField(hint: 'Username',controller: username),
+              const SizedBox(height: 20,),
+              _buildTextField(hint: 'Password', obscureText: true,controller: password),
+              const SizedBox(height: 20,),
+              _buildTextField(hint: 'Full name',controller: fullname),
+              const SizedBox(height: 20,),
+              _buildTextField(hint: 'Email address',controller: emailaddress),
+              const SizedBox(height: 20,),
+              _buildTextField(hint: 'Phone number',controller: phonenumber),
+              const SizedBox(height: 40),
+              ElevatedButton(
                 onPressed: () {
-                  Navigator.of(context).pushNamed('/LoginScreen');
+                  registerUser( username!.text,password!.text,fullname!.text,emailaddress!.text,phonenumber!.text);
+                  username.clear();
+                  password.clear();
+                  fullname.clear();
+                  emailaddress.clear();
+                  phonenumber.clear();
+                  showDialog(
+                    context: context,
+                    builder: (BuildContext context) {
+                      return AlertDialog(
+                        title: Text('Sign up '),
+                        content: Text('Sign up Successfully'),
+                        actions: <Widget>[
+                          TextButton(
+                            onPressed: () {
+                              Navigator.of(context).pop();
+                            },
+                            child: Text('OK'),
+                          ),
+                        ],
+                      );
+                    },
+                  );
                 },
-                child: const Text(
-                  "Login",
-                  style: TextStyle(
-                    fontSize: 18,
-                    color: Colors.blue,
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blue, // Background color
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10.0),
                   ),
-                ))
-          ],
+                  padding: const EdgeInsets.symmetric(vertical: 14.0),
+                ),
+                child: const Text('Start', style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.normal,
+                  color: Colors.white,
+                ),),
+              ),
+              const SizedBox(
+                height: 30,
+              ),
+              const Center(
+                child: Text(
+                  "Already Have an Account?",
+                  style: TextStyle(fontSize: 16, color: Colors.black),
+                ),
+              ),
+              TextButton(
+                  onPressed: () {
+                    Navigator.of(context).pushNamed('/LoginScreen');
+                  },
+                  child: const Text(
+                    "Login",
+                    style: TextStyle(
+                      fontSize: 18,
+                      color: Colors.blue,
+                    ),
+                  ))
+            ],
+          ),
         ),
       ),
     );
   }
 
   Widget _buildTextField({required String hint, bool obscureText = false, TextEditingController? controller}) {
-    return TextField(
+    return TextFormField(
       controller: controller,
       decoration: InputDecoration(
         labelText: hint,
@@ -118,6 +121,17 @@ class SignupScreen extends StatelessWidget {
         ),
         contentPadding: const EdgeInsets.symmetric(vertical: 15.0, horizontal: 10.0),
       ),
+      validator: (value) {
+        if (value!.isEmpty) {
+          return 'Please enter value';
+        }
+        if (value.length < 4) {
+          return 'please enter at least 4 characters';
+        }
+        return null;
+      },
+
+
       obscureText: obscureText,
     );
   }
